8a1e30ce7e3f86ea0318ca2d56f15f6ccc3139a3
Dmitry Fomin <dfgit@ya.ru>
2016-11-14 16:17:40 +0300

    Started project. Main servlet created

---
+++ src/java/org/glassfish/informer/ClientInfo.java
@@ -0,0 +1,54 @@
+package org.glassfish.informer;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+/**
+ *
+ * @author Dmitry Fomin
+ */
+@WebServlet(name = "ClientInfo", urlPatterns = {"/"})
+public class ClientInfo extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setHeader("server", "Informer");
+        response.setContentType("text/html;charset=utf-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println(new StringBuilder("<!DOCTYPE html>"
+                    + "<html>"
+                    + "<head>"
+                    + "<title>Servlet ClientInfo</title>"
+                    + "</head>"
+                    + "<body>"
+                    + "<h1>Servlet ClientInfo at " + request.getContextPath() + "</h1>"
+                    + "</body>"
+                    + "</html>"));
+        }
+    }
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
